b220b7eb9165cbf0bb059acd83162d924660dbe5
Paulo Wells <paulowells@gmail.com>
2024-10-15 13:52:35 -0300

    added select_account prompt to github auth

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 
 export const login = async () => await signIn("github", {
     redirectTo: paths.dashboard.home()
-});
+}, [["prompt", "select_account"]]); /* https://docs.github.com/pt/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps */
 
 export const logout = async () => {
     await signOut();
