f0704b7f79d6152e7b85909816f7bde67427159d
Paulo Wells <paulowells@gmail.com>
2024-12-19 19:48:41 -0300

    updated tags checked state

--- src/components/dashboard/posts/postEdition/tagsEditor/TagsEditor.tsx
+++ src/components/dashboard/posts/postEdition/tagsEditor/TagsEditor.tsx
@@ -5,16 +5,18 @@ import { CheckedStatus } from "../checkedStatus";
 import { Label } from "../label";
 import { Tags } from "@/components/dashboard/tags/Tags";
 import { TagSuggestions } from "@/components/dashboard/tags/TagSuggestions";
+import { useTags } from "@/components/dashboard/tags";
 
 export const TagsEditor = () => {
 
     const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
+    const { tags } = useTags();
 
     return (
         <div className="tags-editor">
             <Label>
                 Tags
-                <CheckedStatus condition={true} />
+                <CheckedStatus condition={tags.length > 0} />
             </Label>
             <Tags
                 showSuggestions={showSuggestions}
